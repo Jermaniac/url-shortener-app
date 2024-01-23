1674fe1bd2b7acdfa2be511faacd908b36b206f8
Diana Perdomo <stalin_bnk@hotmail.com>
2024-01-24 00:54:20 +0100

    chore: added new get request to redirect to the long_url and a post request to create new urls

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { connectToDatabase } = require('./config/db');
 const Url = require('./models/urls');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 connectToDatabase();
 
@@ -15,6 +18,29 @@ app.get('/urls', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+app.get('/:id', async (req, res, next) => {
+  try {
+    const urlFound = await Url.findOne({ short_url: req.params.id });
+    if (urlFound) res.redirect(urlFound.long_url)
+    next()
+  } catch (error) {
+    console.error('Error retrieving URLs:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post("/add", async (req, res) => {
+  // here is the logic for creating a unique shorUrl id
+  const shortUrl = '1234567'
+	const url = new Url({
+		long_url: req.body.long_url,
+    short_url: shortUrl,
+    updated_at: new Date()
+	})
+	await url.save()
+	res.send(url)
+})
 
 app.listen(3000, () => {
   console.log('Express app listening on port 3000');
